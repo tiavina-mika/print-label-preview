495d21f8155693c040e8b43092d917f6322f8afc
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2023-01-19 10:43:28 +0300

    use App.jsx as entry component

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Label from "./Label";
+import { data } from "./utils/data";
+
+const App = () => {
+  return <Label {...data} />;
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
-import Label from "./Label";
-import { data } from "./utils/data";
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <Label {...data} />
+    <App />
   </StrictMode>
 );
