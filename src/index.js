b1a26a09f5999c2cd052b5f93610318e269eea48
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2023-02-22 18:06:39 +0300

    use 70mm size

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 import "./styles/FC90.css";
+// import "./styles/FC70.css";
 import "./styles/base.css";
 import "./styles/preview.css";
 import "./styles/fonts.css";
